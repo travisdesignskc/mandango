060ad851fbcb1adcbf5ca62374fd8779b0fec74f
Travis K <56032877+travisdesignskc@users.noreply.github.com>
2020-04-29 10:02:41 -0500

    master
    JS file update. List's out the seats in an array starting at 0 and returns that the seat is available or not.

--- js/mandango.js
+++ js/mandango.js
@@ -1,1 +1,50 @@
 // JavaScript Document
+
+  var seats = [ false, true, false, true, true, true, false, true, false ];
+  var selSeat = -1;
+
+  function initSeats() {
+	// Initialize the appearance of all seats
+	for (var i = 0; i < seats.length; i++) {
+	  if (seats[i]) {
+		// Set the seat to available
+		// if the value is true in the array then it is available
+		document.getElementById("seat" + i).src = "images/seat_avail.png";
+		document.getElementById("seat" + i).alt = "Available seat";
+	  }
+	  else {
+		// Set the seat to unavailable
+		// if the value is flase in the array then it is not available
+		document.getElementById("seat" + i).src = "images/seat_unavail.png";
+		document.getElementById("seat" + i).alt = "Unavailable seat";
+	  }
+	}
+  }
+
+  function findSeat() {
+	// If seat is already selected, reinitialize all seats to clear them
+	if (selSeat >= 0) {
+	  selSeat = -1;
+	  initSeats();
+	}
+
+	// Search through all the seats for availability
+	for (var i = 0; i < seats.length; i++) {
+	  // See if the current seat is available
+	  if (seats[i]) {
+		// Set the seat selection and update the appearance of the seat
+		selSeat = i;
+		document.getElementById("seat" + i).src = "images/seat_select.png";
+		document.getElementById("seat" + i).alt = "Your seat";
+
+		// Prompt the user to accept the seat
+		var accept = confirm("Seat " + (i + 1) + " is available. Accept?");
+		if (!accept) {
+		  // The user rejected the seat, so clear the seat selection and keep looking
+		  selSeat = -1;
+		  document.getElementById("seat" + i).src = "images/seat_avail.png";
+		  document.getElementById("seat" + i).alt = "Available seat";
+		}
+	  }
+	}
+  }
